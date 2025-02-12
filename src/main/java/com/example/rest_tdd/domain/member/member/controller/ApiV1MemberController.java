63b324f754ebf0db4e6b2754365ee5ba2c0b5a9f
jxxngin <wwd0203@daum.net>
2025-02-12 17:35:52 +0900

    join r/g 4

--- src/main/java/com/example/rest_tdd/domain/member/member/controller/ApiV1MemberController.java
+++ src/main/java/com/example/rest_tdd/domain/member/member/controller/ApiV1MemberController.java
@@ -1,5 +1,7 @@
 package com.example.rest_tdd.domain.member.member.controller;
 
+import com.example.rest_tdd.domain.member.member.dto.MemberDto;
+import com.example.rest_tdd.domain.member.member.entity.Member;
 import com.example.rest_tdd.domain.member.member.service.MemberService;
 import com.example.rest_tdd.global.dto.RsData;
 import lombok.RequiredArgsConstructor;
@@ -22,8 +24,8 @@ public class ApiV1MemberController {
     ) {}
 
     @PostMapping("/join")
-    public RsData<Void> join(@RequestBody JoinReqBody reqBody) {
-        memberService.join(
+    public RsData<MemberDto> join(@RequestBody JoinReqBody reqBody) {
+        Member member = memberService.join(
                 reqBody.username(),
                 reqBody.password(),
                 reqBody.nickname()
@@ -31,7 +33,8 @@ public class ApiV1MemberController {
 
         return new RsData<>(
                 "201-1",
-                "회원 가입이 완료되었습니다."
+                "회원 가입이 완료되었습니다.",
+                new MemberDto(member)
         );
     }
 
