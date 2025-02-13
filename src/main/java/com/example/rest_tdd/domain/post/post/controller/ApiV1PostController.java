9958521223097e4f31ad75ce562a9348bfe16743
jxxngin <wwd0203@daum.net>
2025-02-13 16:43:57 +0900

    [feat] apply post modify

--- src/main/java/com/example/rest_tdd/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/example/rest_tdd/domain/post/post/controller/ApiV1PostController.java
@@ -7,6 +7,8 @@ import com.example.rest_tdd.domain.post.post.service.PostService;
 import com.example.rest_tdd.global.Rq;
 import com.example.rest_tdd.global.dto.RsData;
 import com.example.rest_tdd.global.exception.ServiceException;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -47,6 +49,29 @@ public class ApiV1PostController {
         return new RsData<>(
                 "201-1",
                 "%d번 글 작성이 완료되었습니다.".formatted(post.getId()),
+                new PostDto(post)
+        );
+    }
+
+    record ModifyReqBody(
+            @NotBlank String title,
+            @NotBlank String content
+    ) {}
+
+    @PutMapping("{id}")
+    public RsData<PostDto> modify(@PathVariable long id, @RequestBody @Valid ModifyReqBody reqBody) {
+        Post post = postService.getItem(id).orElseThrow(
+                () -> new ServiceException(
+                        "404-1",
+                        "존재하지 않는 글입니다."
+                )
+        );
+
+        postService.modify(post, reqBody.title(), reqBody.content());
+
+        return new RsData<>(
+                "200-1",
+                "%d번 글 수정이 완료되었습니다.".formatted(id),
                 new PostDto(post)
         );
     }
